1087112fe5de308ed7822df5ffefe65d81f46769
alsedi19 <alsedikadi19@gmail.com>
2025-05-30 15:00:25 +0200

    responsive design phase 3

--- myScript.js
+++ myScript.js
@@ -1,14 +1,28 @@
-        new Glide('.glide_carousel', {
-            type: 'carousel',
-            perView: 3,
-            focusAt: 'center',
-            gap: 20,
-            peek: { before: 50, after: 50 },
-        }).mount();
+new Glide('.glide_carousel', {
+    type: 'carousel',
+    perView: 3,
+    focusAt: 'center',
+    gap: 20,
+    peek: { before: 50, after: 50 },
+    breakpoints: {
+        1024: {
+            perView: 2
+        },
+        768: {
+            perView: 1
+        }
+    }
+}).mount();
 
-        new Glide('.glide_testimonial', {
-            type: 'carousel',
-            perView: 1,
-            focusAt: 'center',
-            peek: { before:0, after: 0 },
-        }).mount();
+new Glide('.glide_testimonial', {
+    type: 'carousel',
+    perView: 1,
+    focusAt: 'center',
+    peek: { before: 0, after: 0 },
+}).mount();
+
+function toggleMenu() {
+    document.querySelectorAll('.nav-list').forEach(list => {
+        list.classList.toggle('show');
+    });
+}
